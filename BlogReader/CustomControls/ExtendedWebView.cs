dfe1a0b16c32c858652b116b449b97d18c7578c9
Mblazeski99 <markoblazeski1999@gmail.com>
2023-01-11 20:01:48 +0100

    added bindable htmlcontent property to webview control

---
+++ BlogReader/CustomControls/ExtendedWebView.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Web.WebView2.Wpf;
+using System.Windows;
+
+namespace BlogReader.CustomControls
+{
+    public class ExtendedWebView : WebView2
+    {
+        public string HTMLContent
+        {
+            get { return (string)GetValue(HTMLContentProperty); }
+            set
+            {
+                SetValue(HTMLContentProperty, value);
+                NavigateToString(HTMLContent);
+            }
+        }
+
+        public static readonly DependencyProperty HTMLContentProperty =
+            DependencyProperty.Register(nameof(HTMLContent), typeof(string), typeof(ExtendedWebView), new PropertyMetadata(string.Empty));
+
+        public ExtendedWebView() : base()
+        {
+            EnsureCreated();
+        }
+
+        private async void EnsureCreated()
+        {
+            await EnsureCoreWebView2Async();
+            NavigateToString(HTMLContent);
+        }
+    }
+}
--- BlogReader/ViewModels/BlogPostItemPreviewViewModel.cs
+++ BlogReader/ViewModels/BlogPostItemPreviewViewModel.cs
@@ -1,17 +1,17 @@
-﻿using BlogReader.Commands;
-using BlogReader.Commands.Blogs.BlogPostItemPreview;
-using BlogReader.Models;
+﻿using BlogReader.Models;
+using BlogReader.Models.Enums;
 using BlogReader.Stores;
+using System;
+using System.IO;
 
 namespace BlogReader.ViewModels
 {
     public class BlogPostItemPreviewViewModel : BaseViewModel
     {
         private readonly BlogPostItem _blogPostItem;
+        private readonly string _htmlContent = string.Empty;
 
-        public BlogPostItem BlogPostItem => _blogPostItem;
-
-        public BaseCommand NavigateToContentCommand { get; }
+        public string HtmlContent => _htmlContent;
 
         public BlogPostItemPreviewViewModel() { }
 
@@ -19,8 +19,68 @@ namespace BlogReader.ViewModels
             NotificationsStore notificationsStore)
         {
             _blogPostItem = blogPostItem;
+            try
+            {
+                string html = string.Empty;
 
-            NavigateToContentCommand = new NavigateToContentCommand(this, notificationsStore);
+                string DisableScriptError = @"<script> 
+                                                function noError() { return true; }
+                                                window.onerror = noError;
+                                              </script>";
+
+                string head = $@"<head>
+                                     {DisableScriptError}
+                                     <meta charset = ""UTF-8"">
+                                 </head>";
+
+                string backgroundColor = AppData.PrimaryBackgroundColorBrush.ToString();
+                string fontColor = AppData.PrimaryFontColorBrush.ToString().Replace("FF", "");
+                string primaryBtnColor = AppData.PrimaryButtonColorBrush.ToString().Replace("FF", "");
+
+                string codeStylesCssFilePath = AppDomain.CurrentDomain.BaseDirectory + @"Assets\Css\preview-blog.css";
+                string codeStylesCss = File.Exists(codeStylesCssFilePath) ? File.ReadAllText(codeStylesCssFilePath) : string.Empty;
+
+                string css = @"<style>
+
+                                " + codeStylesCss + @"
+
+                                body { 
+                                    font: 400 20px/1.25 Metric,Arial,Gadget,sans-serif;
+                                    color: " + fontColor + @";
+                                    background-color: " + backgroundColor + @";
+                                    padding: 10px;
+                                }
+
+                                .blog-post-author { color: " + primaryBtnColor + @"; }
+
+                               </style>";
+
+                string titleHtml = $@"<h1 class=""blog-post-title"">{_blogPostItem.Title}</h1>";
+
+                string postInfoHtml = string.IsNullOrEmpty(_blogPostItem.Author)
+                    ? "<h4>"
+                    : $@"<h4 class=""blog-post-info"">
+                            by: <span class=""blog-post-author"">{_blogPostItem.Author} </span> 
+                            <span class=""blog-post-info-seperator"">|</span>";
+
+                postInfoHtml += $"<span>{_blogPostItem.Date.Value.ToString("MMMM dd, yyyy")}</span> </h4>";
+
+                string mainContent = $@"<body>
+                                            {titleHtml}
+                                            {postInfoHtml}
+                                            {_blogPostItem.Content}
+                                        </body>";
+
+                html = head + css + mainContent;
+
+                _htmlContent = html;
+                OnPropertyChanged(nameof(HtmlContent));
+            }
+            catch (Exception ex)
+            {
+                var error = new Notification(MessageType.Error, "Failed to load blog post", ex.ToString());
+                notificationsStore.AddNotification(error);
+            }
         }
     }
 }
